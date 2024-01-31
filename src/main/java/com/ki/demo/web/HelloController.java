b4dbb397258086867d5de3b897cbad6793dd9af1
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-01-31 10:31:04 -0500

    Verified code working through lecture 8: "Understanding Post Form, Query Params and Path Variables"

--- src/main/java/com/ki/demo/web/HelloController.java
+++ src/main/java/com/ki/demo/web/HelloController.java
@@ -1,15 +1,45 @@
 package com.ki.demo.web;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class HelloController {
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@RequestMapping("/basic")
 	public String sayHello() {
 		return "<h1>hello world</h1>";
+	}
+	
+	@RequestMapping("/proper")
+	public String sayProperHello() {
+		return "<h1>hello there, how are you?</h1>";
+	}
+	
+	@RequestMapping("/user_entry")
+	public String userForm() {
+		return " <form action=\"/greeting/user_greeting\" method=\"GET\">\n" + 
+		" First name:<br>\n" +
+		"  <input type=\"text\" name=\"firstname\">\n" +
+		" <br>\n" +
+		" Last name:<br>\n" + 
+		" <input type=\"text\" name=\"lastname\">\n" + 
+		" <br><br>\n" +
+		" <input type=\"submit\" value=\"Submit\">\n" + 
+		" </form>";
+	}
+	
+	@RequestMapping(value = "/user_greeting", method = RequestMethod.GET)
+	public String printUserGreeting(@RequestParam String firstname, @RequestParam String lastname) {
+		return "Hello there, " + firstname + " " + lastname;
+	}
+	
+	@RequestMapping(value = "/orders/{id}", method = RequestMethod.GET)
+	public String getOrder(@PathVariable String id) {
+		return "Order id: " + id;
 	}
 }
