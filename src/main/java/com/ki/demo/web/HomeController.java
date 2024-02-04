4cccbb3bbb2ea84f688844905775cad039e8b314
Bob Kraemer <bob.kraemer.git@gmail.com>
2024-02-04 08:00:26 -0500

    Verified code working through lecture 13: "Redirects and Data"

--- src/main/java/com/ki/demo/web/HomeController.java
+++ src/main/java/com/ki/demo/web/HomeController.java
@@ -1,9 +1,10 @@
 package com.ki.demo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ki.demo.domain.User;
 
@@ -13,11 +14,20 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("formData", new User());
-		return "index";
+		return "index";  // Looks for a file in your project named index.html to return
 	}
 	
 	@RequestMapping(value="/create", method=RequestMethod.POST)
-	public String processFormData(User user) {
+	public String processFormData(User user, RedirectAttributes attr) {
+		//  Insert data submitted in the form to the database (for example)
+		
+		attr.addFlashAttribute("user", user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping(value="/display", method=RequestMethod.GET)
+	public String displayFormData(User user) {
+		
 		return "result";
 	}
 }
